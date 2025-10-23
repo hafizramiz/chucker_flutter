b7b602124af6c569118223904ed943889dbc0308
Hafiz Ramiz <ramizovhafiz@gmail.com>
2025-10-24 00:21:43 +0300

    updated code

--- lib/src/view/widgets/notification.dart
+++ lib/src/view/widgets/notification.dart
@@ -58,7 +58,7 @@ class _NotificationState extends State<Notification>
   void initState() {
     super.initState();
     Future.delayed(
-      Duration(seconds: 30),
+      Duration(seconds: 5),
       () {
         if (mounted) _controller.reverse();
       },
