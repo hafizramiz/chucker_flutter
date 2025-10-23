8af8cc4c47615f7bff2ea5c2ecf537e06df95800
Hafiz Ramiz <ramizovhafiz@gmail.com>
2025-10-23 23:22:22 +0300

    http login interceptor updated

--- lib/src/interceptors/http_logging_interceptor.dart
+++ lib/src/interceptors/http_logging_interceptor.dart
@@ -4,23 +4,28 @@ import 'package:chopper/chopper.dart';
 import 'package:chucker_flutter/src/loggers/logger.dart';
 import 'package:http/http.dart' as http;
 
-///Logs http request and response data
+////Logs http request and response data
+
 class ChuckerHttpLoggingInterceptor implements Interceptor {
   @override
   FutureOr<Response<BodyType>> intercept<BodyType>(
-    Chain<BodyType> chain,
-  ) async {
+      Chain<BodyType> chain,
+      ) async {
     final requestBase = await chain.request.toBaseRequest();
+
     Logger.request('${requestBase.method} ${requestBase.url}');
+
     requestBase.headers.forEach((k, v) => Logger.request('$k: $v'));
 
     var bytes = '';
+
     if (requestBase is http.Request) {
-      final req = requestBase as http.Request;
-      final body = req.body;
+      final body = (requestBase as http.Request).body;
+
       if (body.isNotEmpty) {
         Logger.json(body, isRequest: true);
-        bytes = ' (${req.bodyBytes.length}-byte body)';
+
+        bytes = ' (${body.length}-byte body)';
       }
     }
 
@@ -29,20 +34,25 @@ class ChuckerHttpLoggingInterceptor implements Interceptor {
     final response = await chain.proceed(chain.request);
 
     final base = response.base.request;
+
     Logger.response('${response.statusCode} ${base!.url}');
 
     response.base.headers.forEach((k, v) => Logger.response('$k: $v'));
 
     var responseBytes = '';
+
     if (response.base is http.Response) {
       final resp = response.base as http.Response;
+
       if (resp.body.isNotEmpty) {
         Logger.json(resp.body);
+
         responseBytes = ' (${response.bodyBytes.length}-byte body)';
       }
     }
 
     Logger.response('END ${base.method}$responseBytes');
+
     return response;
   }
 }
